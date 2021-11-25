3b35e9428f375142d55250b669eff9601bf52f7a
P33J3 <peterjonlawrence@gmail.com>
2021-11-25 00:48:22 -0500

    React Redux Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,26 +3,39 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
-import { CAMPSITES } from "../shared/campsites";
-import { COMMENTS} from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
+//State is now managed by Redux
+// import { CAMPSITES } from "../shared/campsites";
+// import { COMMENTS} from '../shared/comments';
+// import { PARTNERS } from '../shared/partners';
+// import { PROMOTIONS } from '../shared/promotions';
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import About from './AboutComponent';
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from 'react-redux';
 
-class Main extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			campsites: CAMPSITES,
-      comments: COMMENTS,
-      partners: PARTNERS,
-      promotions: PROMOTIONS
-			// selectedCampsite: null
-		};
+
+// Use the following function to forward state and change all occurences of 'state' to 'props' in the component below
+const mapStateToProps = state => {
+	return {
+		campsites: state.campsites,
+		comments: state.comments,
+		partners: state.partners,
+		promotions: state.promotions
 	}
+}
+class Main extends Component {
+	//State is now managed by Redux
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	// 		campsites: CAMPSITES,
+    //   comments: COMMENTS,
+    //   partners: PARTNERS,
+    //   promotions: PROMOTIONS
+	// 		// selectedCampsite: null
+	// 	};
+	// }
 
 	// onCampsiteSelect(campsiteId) {
 	//     this.setState({selectedCampsite: campsiteId});
@@ -34,17 +47,17 @@ class Main extends Component {
     // We use the arrow function here as it is able to pull scope from its parent. A function declaration would have only been locally scoped
 		const HomePage = () => {
 			return <Home
-                campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
-                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                partner={this.state.partners.filter(partner => partner.featured)[0]}
+                campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
+                promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+                partner={this.props.partners.filter(partner => partner.featured)[0]}
                  />;
 		};
 
     const CampsiteWithId =  ({match}) => {
       return (
         <CampsiteInfo
-           campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-        comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+           campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+        comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
          />
   
       )
@@ -63,11 +76,11 @@ class Main extends Component {
 					<Route
 						exact
 						path="/directory"
-						render={() => <Directory campsites={this.state.campsites} />}
+						render={() => <Directory campsites={this.props.campsites} />}
 					/>
-          {/*When passing state data use render syntax; otherwise use component attribute*/}
+          {/*When passing props data use render syntax; otherwise use component attribute*/}
           <Route path='/directory/:campsiteId' component={CampsiteWithId}  />
-		  <Route path='/aboutus' render={ () => <About partners={this.state.partners} /> } />
+		  <Route path='/aboutus' render={ () => <About partners={this.props.partners} /> } />
           <Route exact path='/contactus' component={Contact} />
 					<Redirect to="/home" />
 				</Switch>
@@ -84,4 +97,5 @@ class Main extends Component {
 	}
 }
 
-export default Main;
+//connect helps to link the redux and withRouter allows RouterDom to still work when using Redux
+export default withRouter(connect(mapStateToProps)(Main));
